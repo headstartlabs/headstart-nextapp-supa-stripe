5306cb7628aab9e35ccd934e3003ecaff9e0be3c
headstartlabs <headstart.labs@gmail.com>
2024-01-04 21:38:27 +1100

    Add webhooks

---
+++ actions/stripe/create-portal-link.ts
@@ -0,0 +1,47 @@
+import { cookies, headers } from 'next/headers';
+import { createClient } from "@/utils/supabase/server";
+import { stripe } from '@/utils/stripe/server';
+import { createOrRetrieveCustomer } from '@/utils/supabase/supabase-admin';
+import { getURL } from '@/utils/helpers';
+import { Database } from '@/types/TSupabaseDatabase';
+
+
+export async function POST(req: Request) {
+  if (req.method === 'POST') {
+    try {
+      const cookieStore = cookies();
+      const supabase = createClient(cookieStore);
+      const {
+        data: { user }
+      } = await supabase.auth.getUser();
+
+      if (!user) throw Error('Could not get user');
+      const customer = await createOrRetrieveCustomer({
+        uuid: user.id || '',
+        email: user.email || ''
+      });
+
+      if (!customer) throw Error('Could not get customer');
+      const { url } = await stripe.billingPortal.sessions.create({
+        customer,
+        return_url: `${getURL()}/account`
+      });
+      return new Response(JSON.stringify({ url }), {
+        status: 200
+      });
+    } catch (err: any) {
+      console.log(err);
+      return new Response(
+        JSON.stringify({ error: { statusCode: 500, message: err.message } }),
+        {
+          status: 500
+        }
+      );
+    }
+  } else {
+    return new Response('Method Not Allowed', {
+      headers: { Allow: 'POST' },
+      status: 405
+    });
+  }
+}
--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,6 +1,8 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { type NextRequest, NextResponse } from 'next/server'
 
+import { Database } from '@/types/TSupabaseDatabase'
+
 export const createClient = (request: NextRequest) => {
   // Create an unmodified response
   let response = NextResponse.next({
@@ -9,7 +11,7 @@ export const createClient = (request: NextRequest) => {
     },
   })
 
-  const supabase = createServerClient(
+  const supabase = createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
